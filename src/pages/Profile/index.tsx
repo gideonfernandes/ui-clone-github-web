76f3013ac9171d651171e84422ffb593d99df31c
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-12 16:34:50 -0300

    Add ProfileData component on Profile page component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Main, LeftSide, RightSide } from './styles';
+
+import ProfileData from '../../components/ProfileData';
 
 const Profile: React.FC = () => {
   return (
     <Container>
-      <h1>Profile</h1>
+      <Main>
+        <LeftSide>
+          <ProfileData
+            username={'gideonfernandes'}
+            name={'Gideon Fernandes'}
+            avatarUrl={'https://avatars2.githubusercontent.com/u/62628971?v=4'}
+            followers={560}
+            following={8}
+            company={'Lince Net'}
+            location={'Araras/SP, Brasil'}
+            email={'gideon.de.fernandes@gmail.com'}
+            blog={'linkedin.com/in/gideonfernandes'}
+          />
+        </LeftSide>
+
+        <RightSide></RightSide>
+      </Main>
     </Container>
   );
 };
