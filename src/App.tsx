b742ac3fad7f8b0e5c6cfacbd6b5a1744f9c642d
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-12 14:56:18 -0300

    Add routes setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+import Profile from './pages/Profile'
+import Repository from './pages/Repository'
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>Rocketseat</h1>
+    <BrowserRouter>
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Profile />} />
+        <Route path="/:username" element={<Profile />} />
+        <Route path="/:username/:reponame" element={<Repository />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Repository: React.FC = () => {
+  return (
+    <Container>
+      <h1>Repository</h1>
+    </Container>
+  );
+};
+
+export default Repository;
