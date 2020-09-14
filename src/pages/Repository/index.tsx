8726fe696092c276c8c09cb9457c6d22e8993233
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-14 14:52:55 -0300

    Add Repository page components & styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,11 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-import { Container } from './styles';
+import {
+  Container,
+  Breadcrumb,
+  RepoIcon,
+  Stats,
+  StarIcon,
+  ForkIcon,
+  LinkButton,
+  GithubIcon,
+} from './styles';
 
 const Repository: React.FC = () => {
   return (
     <Container>
-      <h1>Repository</h1>
+      <Breadcrumb>
+        <RepoIcon />
+
+        <Link className={'username'} to={'/gideonfernandes'}>
+          gideonfernandes
+        </Link>
+
+        <span>/</span>
+
+        <Link className={'reponame'} to={'/gideonfernandes/contact-keeper'}>
+          contact-keeper
+        </Link>
+      </Breadcrumb>
+
+    <p>Create an account & store your contact list</p>
+
+    <Stats>
+      <li>
+        <StarIcon />
+        <strong>9</strong>
+        <span>Stats</span>
+      </li>
+
+      <li>
+        <ForkIcon />
+        <strong>4</strong>
+        <span>forks</span>
+      </li>
+    </Stats>
+
+    <LinkButton href={'https://github.com/gideonfernandes/contact-keeper'}>
+      <GithubIcon />
+      <span>Ver no Github</span>
+    </LinkButton>
     </Container>
   );
 };
