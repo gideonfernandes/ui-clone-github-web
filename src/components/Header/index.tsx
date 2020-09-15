3d16ec3fc7dfebe27959ca8a33ce0a52e72968a3
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-15 16:43:39 -0300

    Add search user functionality

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { Container, GithubLogo, SearchForm } from './styles';
 
 const Header: React.FC = () => {
+  const [search, setSearch ] = useState('');
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    
+    navigate('/' + search.toLowerCase().trim());
+  };
+
   return (
     <Container>
       <GithubLogo />
-      <SearchForm onSubmit={() => {}}>
-        <input type="text" placeholder="Digite um username ou reponame" />
-
+      <SearchForm onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite um username ou reponame"
+          value={search}
+          onChange={(event) => setSearch(event.currentTarget.value)}
+        />
       </SearchForm>
     </Container>
   );
