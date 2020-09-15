d2e721e9a5ddc2c5080907ff314efc5d42bd5b5c
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-15 17:16:20 -0300

    Add dark mode with styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 
 import 'react-calendar-heatmap/dist/styles.css';
 
-import React from 'react';
+import React, { useState } from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Profile from './pages/Profile'
 import Repository from './pages/Repository'
@@ -10,22 +11,28 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import GlobalStyles from './styles/GlobalStyles';
+import { ThemeName, themes } from './styles/themes';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <GlobalStyles />
-      
-      <Header />
-      
-      <Routes>
-        <Route path="/" element={<Profile />} />
-        <Route path="/:username" element={<Profile />} />
-        <Route path="/:username/:reponame" element={<Repository />} />
-      </Routes>
+  const [themeName, setThemeName] = useState<ThemeName>('light');
+  const currentTheme = themes[themeName];
 
-      <Footer />
-    </BrowserRouter>
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <BrowserRouter>
+        <GlobalStyles />
+        
+        <Header themeName={themeName} setThemeName={setThemeName} />
+        
+        <Routes>
+          <Route path="/" element={<Profile />} />
+          <Route path="/:username" element={<Profile />} />
+          <Route path="/:username/:reponame" element={<Repository />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 
