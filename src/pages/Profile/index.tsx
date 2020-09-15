ad71aa69794ca4952208aa5190f23a5dc87aed8b
Gideon Fernandes <gideon.de.fernandes@gmail.com>
2020-09-15 16:37:33 -0300

    Add github real data on Profile page component

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import {
   Container,
@@ -14,13 +15,56 @@ import {
 import ProfileData from '../../components/ProfileData';
 import RepositoryCard from '../../components/RepositoryCard';
 import RandomCalendar from '../../components/RandomCalendar';
+import { APIUser, APIRepo } from '../../@types';
+
+interface Data {
+  user?: APIUser;
+  repos?: APIRepo[];
+  error?: string;
+}
 
 const Profile: React.FC = () => {
+  const { username = 'gideonfernandes' } = useParams();
+  const [data, setData] = useState<Data>();
+
+  useEffect(() => {
+    Promise.all([
+      fetch(`https://api.github.com/users/${username}`),
+      fetch(`https://api.github.com/users/${username}/repos`),
+    ]).then(async (responses) => {
+      const [userResponse, reposResponse] = responses;
+
+      if (userResponse.status === 404) {
+        setData({ error: 'Usuário não encontrado!' });
+        return;
+      }
+
+      const user = await userResponse.json();
+      const repos = await reposResponse.json();
+
+      const shuffledRepos = repos.sort(() => 0.50 - Math.random());
+      const slicedRepos = shuffledRepos.slice(0, 6);
+
+      setData({
+        user,
+        repos: slicedRepos,
+      });
+    });
+  }, [username]);
+
+  if (data?.error) {
+    return <h1>{data.error}</h1>
+  }
+
+  if (!data?.user || !data?.repos) {
+    return <h1>Loading...</h1>
+  }
+
   const TabContent = () => (
     <div className="content">
       <RepoIcon />
       <span className="label">Repositórios</span>
-      <span className="number">26</span>
+      <span className="number">{data.user?.public_repos}</span>
     </div>
   );
 
@@ -36,15 +80,15 @@ const Profile: React.FC = () => {
       <Main>
         <LeftSide>
           <ProfileData
-            username={'gideonfernandes'}
-            name={'Gideon Fernandes'}
-            avatarUrl={'https://avatars2.githubusercontent.com/u/62628971?v=4'}
-            followers={560}
-            following={8}
-            company={'Lince Net'}
-            location={'Araras/SP, Brasil'}
-            email={'gideon.de.fernandes@gmail.com'}
-            blog={'linkedin.com/in/gideonfernandes'}
+            username={data.user.login}
+            name={data.user.name}
+            avatarUrl={data.user.avatar_url}
+            followers={data.user.followers}
+            following={data.user.following}
+            company={data.user.company}
+            location={data.user.location}
+            email={data.user.email}
+            blog={data.user.blog}
           />
         </LeftSide>
 
@@ -58,15 +102,15 @@ const Profile: React.FC = () => {
             <h2>Repositórios aleatórios</h2>
 
             <div>
-              {[1, 2, 3, 4, 5, 6].map(number => (
+              {data.repos.map((repo) => (
                 <RepositoryCard
-                  key={number}
-                  username={'gideonfernandes'}
-                  reponame={'contact-keeper'}
-                  description={'Create an account & store your contact list'}
-                  language={number % 3 === 0 ? 'Javascript' : 'Typescript'}
-                  stars={8}
-                  forks={4}
+                  key={repo.name}
+                  username={repo.owner.login}
+                  reponame={repo.name}
+                  description={repo.description}
+                  language={repo.language}
+                  stars={repo.stargazers_count}
+                  forks={repo.forks}
                 />
               ))}
             </div>
